0240699bc4a257d4e49e3b0beba174bf93df9d8f
brooksjbr <brooksjbr@protonmail.ch>
2025-09-16 19:15:12 -0400

    feat: add workflow

--- test/test_hello_world.py
+++ test/test_hello_world.py
@@ -1,8 +1,8 @@
-from src.hello_world import print_msg
+from src.hello_world import msg
 
 
 def test_hello_world():
-    msg = "Hello World!"
-    result = print_msg(msg)
+    my_msg = "Hello World!"
+    result = msg(my_msg)
 
     assert result == "Your message is Hello World!"
